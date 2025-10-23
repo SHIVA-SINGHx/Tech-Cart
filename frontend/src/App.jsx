c966d063c9a8211d552d0d96a88f1c7294a77a8b
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-10-23 11:18:00 -0700

    Shadcn ui fixes now it is working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import { ChartBar } from 'lucide-react'
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
     <div className='bg-red-50'>
       this is home page
-      
+      <Button>Click me</Button>
     </div>
   )
 }
