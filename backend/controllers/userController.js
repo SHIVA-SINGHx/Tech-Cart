fbeda05e3f1bbd09502fb898d13d25bdb492a676
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-10-23 08:03:19 -0700

    Created allUser route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -378,3 +378,19 @@ export const changePassword = async (req, res) => {
     });
   }
 };
+
+export const allUser = async(req, res)=>{
+  try {
+    const users = await User.find()
+    return res.status(200).json({
+      success: true,
+      users
+    })
+    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { changePassword, forgotPassword, login, logout, register, reVerify, verify, verifyOTP } from "../controllers/userController.js"
+import { allUser, changePassword, forgotPassword, login, logout, register, reVerify, verify, verifyOTP } from "../controllers/userController.js"
 import { isAuthenticated } from "../middleware/isAuthenticated.js"
 
 const router = express.Router()
@@ -12,5 +12,6 @@ router.post("/logout", isAuthenticated, logout)
 router.post("/forgot-password", forgotPassword)
 router.post("/verify-otp/:email", verifyOTP)
 router.post("/change-password/:email", changePassword)
+router.get("/all-user", allUser)
 
 export default router
