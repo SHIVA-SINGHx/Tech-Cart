f697a6e9225d174ac5e4cc94d822da93157fdda2
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-05 09:36:14 -0800

    created getProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,28 +1,58 @@
-import express from "express"
+import express from "express";
+import getDataUri from "../utils/dataUri";
+import cloudniary from "cloudinary"
+import { Product } from "../models/productModel";
 
-export const getProduct = async (req, res) =>{
-    try {
-        const {productName, productDescription, price, category, brand} = req.body;
-        const userId = req.id;
+export const getProduct = async (req, res) => {
+  try {
+    const { productName, productDescription, price, category, brand } =
+      req.body;
+    const userId = req.id;
 
-        if(!productName || !productDescription || !price || !category || !brand){
-            return res.status(400).json({
-                success: false,
-                message: "All fields are required"
-            })
-        }
-
-        // handle multiple images
-        let productImg = [];
-        if(req.files && req.files.length > 0){
-
-        }
-
-
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        })
+    if (!productName || !productDescription || !price || !category || !brand) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
-}
+
+    // handle multiple images
+    let productImg = []; 
+    if (req.files && req.files.length > 0) {
+      for (const file of req.files) {
+          const fileUri = getDataUri(file)
+          const result = await cloudniary.upload(fileUri,{
+            folder: "mern_products" // folder name of cloudinary
+          })
+
+          productImg.push({
+            url: result.secure_url,
+            public_id: result.public_id
+          })
+      }
+    }
+
+    const newProduct = await Product.create({
+        userId,
+        productName,
+        productDescription,
+        price,
+        brand,
+        category,
+        productImg  // array of objects 
+    })
+
+    return res.status(200).json({
+        success: true,
+        message: "Product created successfully",
+        product: newProduct
+    })
+
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
