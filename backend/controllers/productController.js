228424b29d5890d2200f4b318e9a206948e42580
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-05 09:14:06 -0800

    created productController

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+import express from "express"
+
+export const getProduct = async (req, res) =>{
+    try {
+        const {productName, productDescription, price, category, brand} = req.body;
+        const userId = req.id;
+
+        if(!productName || !productDescription || !price || !category || !brand){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        // handle multiple images
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
